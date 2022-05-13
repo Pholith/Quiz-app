a166a4e7c3e95475801dda2aae73b78503b63672
Pholith <vincent22062@gmail.com>
2022-05-13 15:32:51 +0200

    login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import jwt_utils
 
 app = Flask(__name__)
 
@@ -10,6 +11,14 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+	
+@app.route('/login', methods=['POST'])
+def Login():
+	payload = request.get_json()
+	if (payload['password'] == 'admin'):
+		return {"token": jwt_utils.build_token()}, 200
+	return {}, 401
 
 	
 if __name__ == "__main__":

31ced7c0f655505dd9f09c84d5a6dd0f728d4518
Pholith <vincent22062@gmail.com>
2022-05-31 13:52:37 +0200

    Fin composant HomePage

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,18 +1,30 @@
 <template>
   <h1>Home page</h1>
+
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
+  <router-link to="/start-new-quiz-page">Démarrer le quiz !</router-link>
 </template>
 
-<script>
-import quizApiService from "@/services/quizApiService";
+
+
+<script lang="ts">
+var registeredScores: number[] = [];
 
 export default {
-  name: "HomePage",
   data() {
     return {
+      registeredScores
     };
   },
+
   async created() {
     console.log("Composant Home page 'created'");
   }
 };
 </script>
+
+
+<style>
+</style>

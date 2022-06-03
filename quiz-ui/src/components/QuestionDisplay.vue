1b7429335fc39ef8f1dba658b59332ef5ff7d455
Katastorm <j.faidide@yahoo.com>
2022-06-03 16:58:22 +0200

    Update QuestionDisplay.vue

--- quiz-ui/src/components/QuestionDisplay.vue
+++ quiz-ui/src/components/QuestionDisplay.vue
@@ -1,6 +1,9 @@
 <template>
   <img v-if="question.image" :src="question.image" />
-  <a @click="$emit('answer-selected', 2)">La réponse D</a>
+  <a @click="$emit('answer-selected', 0)">La réponse A</a>
+  <a @click="$emit('answer-selected', 1)">La réponse B</a>
+  <a @click="$emit('answer-selected', 2)">La réponse C</a>
+  <a @click="$emit('answer-selected', 3)">La réponse D</a>
 
 </template>
 
